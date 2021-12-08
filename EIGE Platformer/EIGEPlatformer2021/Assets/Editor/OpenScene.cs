02036d6f99b8c9356a73b38a1d11c0f3c06664e2
Landvogt <34479740+Landvogt@users.noreply.github.com>
2021-12-08 08:50:49 +0100

    Week7 Tutorial Gizmos, Menu Button

---
+++ EIGE Platformer/EIGEPlatformer2021/Assets/Editor/OpenScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class OpenScene : MonoBehaviour
+{
+    [MenuItem("OpenScene/Start Screen")]
+    static void StartScreen()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Scenes/StartScreen.unity");
+    }
+}
